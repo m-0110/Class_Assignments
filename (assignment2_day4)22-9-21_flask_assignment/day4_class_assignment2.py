f1544cc4e9045ea92aacb184626245bc7e02791d
m-0110 <63664340+m-0110@users.noreply.github.com>
2021-10-01 13:56:54 +0530

    Add files via upload

---
+++ (assignment2_day4)22-9-21_flask_assignment/day4_class_assignment2.py
@@ -0,0 +1,35 @@
+'''
+PROGRAM DESCRIPTION:
+create three end points and add css and javascript
+'''
+
+
+
+#PROGRAMMED BY: Modika Ishwarya
+#email-id:b18cs002@kitsw.ac.in
+#DATE:21-09-2021
+#PYTHON VERSION:3.8
+#CAVEATS:None
+#LICENSE:None
+
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/register')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
